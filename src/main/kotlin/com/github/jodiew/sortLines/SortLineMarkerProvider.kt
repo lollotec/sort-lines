a34720323577c8c276005a3b1256b75ee4bb026b
Jodie Weldon <weldon.jodie@gmail.com>
2025-07-20 14:47:42 -0700

    Refactor test files

---
+++ src/main/kotlin/com/github/jodiew/sortLines/SortLineMarkerProvider.kt
@@ -0,0 +1,4 @@
+package com.github.jodiew.sortLines
+
+class SortLineMarkerProvider {
+}
---
+++ src/test/kotlin/com/github/jodiew/sortLines/LineOrderInspectionHighlightingTests.kt
@@ -0,0 +1,73 @@
+package com.github.jodiew.sortLines
+
+import com.intellij.testFramework.TestDataPath
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+@TestDataPath($$"$CONTENT_ROOT/testData")
+class LineOrderInspectionHighlightingTests: BasePlatformTestCase() {
+
+    protected override fun setUp() {
+        super.setUp()
+        myFixture.enableInspections(LineOrderInspection())
+    }
+
+    protected override fun getTestDataPath(): String =
+        "src/test/testData/highlighting"
+
+    private fun highlightingTestKotlin(name: String) {
+        myFixture.testHighlighting(
+            true,
+            false,
+            false,
+            testFileNameKt(name)
+        )
+    }
+
+    fun testNoSortHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testSimpleLinesUnsortedHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testSimpleLinesSortedNoHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testMultipleLinesUnsortedHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testNoEndCommentHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testNoStartCommentHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testNoEndMultipleCommentHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testNoStartMultipleCommentHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testMismatchedEndCommentHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun testInvalidSortOptionHighlighting() {
+        highlightingTestKotlin(name)
+    }
+
+    fun `test simple quick fix`() {
+        myFixture.configureByFile("SimpleLinesUnsorted_before.kt")
+        val sortFixes = myFixture.getAllQuickFixes().filter { it.text == QUICK_FIX_NAME }
+        assertFalse(sortFixes.isEmpty())
+        sortFixes.forEach { myFixture.launchAction(it) }
+        myFixture.checkResultByFile("SimpleLinesUnsorted_after.kt")
+    }
+}
