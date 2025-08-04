980943aa194d0e234aef951b3c2cf354fbc9b0e4
Jodie Weldon <weldon.jodie@gmail.com>
2025-08-04 11:56:28 -0700

    Add simple sort completion

---
+++ src/main/kotlin/com/github/jodiew/sortlines/lang/SortCompletionContributor.kt
@@ -0,0 +1,33 @@
+package com.github.jodiew.sortlines.lang
+
+import com.github.jodiew.sortlines.DEFAULT_ASC
+import com.github.jodiew.sortlines.DEFAULT_DESC
+import com.github.jodiew.sortlines.lang.psi.SortTypes
+import com.intellij.codeInsight.completion.CompletionContributor
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProvider
+import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.util.ProcessingContext
+
+class SortCompletionContributor : CompletionContributor() {
+    init {
+        extend(
+            CompletionType.BASIC,
+            PlatformPatterns.psiElement(SortTypes.SORT),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    resultSet: CompletionResultSet,
+                ) {
+                    (DEFAULT_ASC + DEFAULT_DESC).forEach { sort ->
+                        resultSet.addElement(LookupElementBuilder.create(sort))
+                    }
+                }
+            }
+        )
+    }
+}
