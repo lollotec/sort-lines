11e5d87fb8fea91dd6f1b64afc9c3cb9b8444364
Jodie Weldon <weldon.jodie@gmail.com>
2025-08-09 12:44:56 -0700

    Fix and cleanup sort annotator

--- src/main/kotlin/com/github/jodiew/sortlines/lang/SortAnnotator.kt
+++ src/main/kotlin/com/github/jodiew/sortlines/lang/SortAnnotator.kt
@@ -6,8 +6,8 @@ import com.github.jodiew.sortlines.VALID_SORTS
 import com.github.jodiew.sortlines.isSortComment
 import com.github.jodiew.sortlines.lang.colors.SortColor
 import com.github.jodiew.sortlines.lang.psi.SortOptions
-import com.github.jodiew.sortlines.lang.psi.ext.isEnd
-import com.github.jodiew.sortlines.lang.psi.ext.sort
+import com.github.jodiew.sortlines.lang.psi.ext.end
+import com.github.jodiew.sortlines.lang.psi.ext.order
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
@@ -19,7 +19,6 @@ import com.intellij.psi.PsiElement
 class SortAnnotator: Annotator {
 
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-
         // Check if the PSI element is a comment and contains the sort prefix
         if (element is PsiComment && element.isSortComment()) {
             // Define the text ranges (start is inclusive, end is exclusive)
@@ -39,10 +38,10 @@ class SortAnnotator: Annotator {
         // Check if the psi element is SortOptions
         if (element is SortOptions) {
             // Check if it's a block end
-            if (element.isEnd) return
+            if (element.end) return
 
             // Check the sort order
-            if (element.sort !in VALID_SORTS) {
+            if (element.order !in VALID_SORTS) {
                 holder.newAnnotation(HighlightSeverity.ERROR, "Invalid sort order")
                     .range(element.textRange)
                     .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
--- src/main/kotlin/com/github/jodiew/sortlines/lang/psi/ext/SortOptions.kt
+++ src/main/kotlin/com/github/jodiew/sortlines/lang/psi/ext/SortOptions.kt
@@ -1,31 +1,10 @@
 package com.github.jodiew.sortlines.lang.psi.ext
 
-import com.github.jodiew.sortlines.lang.psi.SortElementFactory
 import com.github.jodiew.sortlines.lang.psi.SortOptions
 import com.github.jodiew.sortlines.lang.psi.SortTypes
 
-var SortOptions.sort: String?
-    get() = node.findChildByType(SortTypes.SORT)?.text
-    set(value) {
-        val sortNode = node.findChildByType(SortTypes.SORT)
-        if (sortNode != null) {
-            val options = SortElementFactory.createSortOptions(project, value)
-            val newSortNode = options.firstChild.node
-            node.replaceChild(sortNode, newSortNode)
-        }
-    }
-
-val SortOptions.isEnd: Boolean
+val SortOptions.end: Boolean
     get() = node.findChildByType(SortTypes.END) != null
 
-val SortOptions.isGroup: Boolean
-    get() = node.findChildByType(SortTypes.GROUP) != null
-
-val SortOptions.isSplit: Boolean
-    get() = node.findChildByType(SortTypes.SPLIT) != null
-
-val SortOptions.pattern: String?
-    get() = node.findChildByType(SortTypes.PATTERN)?.text
-
-val SortOptions.index: Int?
-    get() = node.findChildByType(SortTypes.INDEX)?.text?.toInt()
+val SortOptions.order: String?
+    get() = sort?.text
