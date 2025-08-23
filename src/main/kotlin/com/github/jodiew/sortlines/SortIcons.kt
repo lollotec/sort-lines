764e23bfc4879f941afce86a6110f76cb77c3394
Jodie Weldon <weldon.jodie@gmail.com>
2025-08-23 16:32:07 -0700

    Clean up icons and add light versions

--- src/main/kotlin/com/github/jodiew/sortlines/SortIcons.kt
+++ src/main/kotlin/com/github/jodiew/sortlines/SortIcons.kt
@@ -6,9 +6,9 @@ object SortIcons {
     @JvmField
     val FILE = IconLoader.getIcon("/icons/jar-gray.png", javaClass)
     @JvmField
-    val AscSort = IconLoader.getIcon("/icons/ascSort_dark.svg", javaClass)
+    val AscSort = IconLoader.getIcon("/icons/gutter/ascSort.svg", javaClass)
     @JvmField
-    val DescSort = IconLoader.getIcon("/icons/descSort_dark.svg", javaClass)
+    val DescSort = IconLoader.getIcon("/icons/gutter/descSort.svg", javaClass)
     @JvmField
-    val NoSort = IconLoader.getIcon("/icons/noSort_dark.svg", javaClass)
+    val NoSort = IconLoader.getIcon("/icons/gutter/noSort.svg", javaClass)
 }
