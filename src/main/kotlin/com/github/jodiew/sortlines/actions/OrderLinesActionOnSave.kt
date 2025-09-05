ab4e065e926f0c7cb48abe8af7bfcf4605e7d6dc
Jodie Weldon <weldon.jodie@gmail.com>
2025-09-05 11:06:46 -0700

    Add sort lines to the Actions on save

---
+++ src/main/kotlin/com/github/jodiew/sortlines/actions/OrderLinesActionOnSave.kt
@@ -0,0 +1,50 @@
+package com.github.jodiew.sortlines.actions
+
+import com.github.jodiew.sortlines.SortBundle
+import com.github.jodiew.sortlines.lang.psi.forEachSort
+import com.github.jodiew.sortlines.settings.OrderLinesActionOnSaveInfoProvider
+import com.intellij.ide.actionsOnSave.impl.ActionsOnSaveFileDocumentManagerListener
+import com.intellij.openapi.application.readAndEdtWriteAction
+import com.intellij.openapi.command.executeCommand
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
+
+class OrderLinesActionOnSave: ActionsOnSaveFileDocumentManagerListener.DocumentUpdatingActionOnSave() {
+    override val presentableName: String
+        get() = SortBundle.message("action.com.github.jodiew.sortlines.on.save.name.sentence")
+
+    override fun isEnabledForProject(project: Project): Boolean =
+        OrderLinesActionOnSaveInfoProvider().enabledOnSave(project)
+
+    override suspend fun updateDocument(project: Project, document: Document) {
+        readAndEdtWriteAction {
+            val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document)
+            if (psiFile == null) {
+                thisLogger().warn("no psi file")
+                return@readAndEdtWriteAction writeAction {  }
+            }
+
+            writeAction {
+                psiFile.forEachSort(document) { info, range ->
+                    val sortedLines = info.sorted(document.getText(range).lines())?.joinToString("\n")
+                    if (sortedLines == null) {
+                        thisLogger().warn("Something went wrong with the sort")
+                        return@forEachSort
+                    }
+                        executeCommand(
+                            project,
+                            SortBundle.message("action.com.github.jodiew.sortlines.on.save.name.title"
+                            )) {
+                            document.replaceString(
+                                range.startOffset,
+                                range.endOffset,
+                                sortedLines
+                            )
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/jodiew/sortlines/settings/OrderLinesActionOnSaveInfoProvider.kt
@@ -0,0 +1,28 @@
+package com.github.jodiew.sortlines.settings
+
+import com.github.jodiew.sortlines.SortBundle
+import com.intellij.codeInsight.actions.onSave.ActionOnSaveInfoBase
+import com.intellij.ide.actionsOnSave.ActionOnSaveContext
+import com.intellij.ide.actionsOnSave.ActionOnSaveInfo
+import com.intellij.ide.actionsOnSave.ActionOnSaveInfoProvider
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.project.Project
+
+
+class OrderLinesActionOnSaveInfoProvider: ActionOnSaveInfoProvider() {
+    override fun getActionOnSaveInfos(context: ActionOnSaveContext): Collection<ActionOnSaveInfo?> =
+        listOf(object : ActionOnSaveInfoBase(
+            context,
+            SortBundle.message("action.com.github.jodiew.sortlines.on.save.name.sentence"),
+            SORT_LINES_ON_SAVE,
+            SORT_LINES_BY_DEFAULT,
+        ){})
+
+    fun enabledOnSave(project: Project) =
+        PropertiesComponent.getInstance(project).getBoolean(SORT_LINES_ON_SAVE, SORT_LINES_BY_DEFAULT)
+
+    companion object {
+        private const val SORT_LINES_ON_SAVE = "sort.lines.on.save"
+        private const val SORT_LINES_BY_DEFAULT = true
+    }
+}
