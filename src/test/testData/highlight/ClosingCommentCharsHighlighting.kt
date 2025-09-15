f6019937d3291e31ae0d357aca96afcbb8dd15c1
Jodie Weldon <weldon.jodie@gmail.com>
2025-09-15 13:32:52 -0700

    Fix comment closing characters being included in sort language injection

---
+++ src/test/testData/highlight/ClosingCommentCharsHighlighting.kt
@@ -0,0 +1,7 @@
+/* sort: asc */
+<warning descr="Lines not in specified order">const val bee = "second"
+const val ant = "first"</warning>
+
+/* sort: { order: asc } */
+<warning descr="Lines not in specified order">const val berry = "second"
+const val apple = "first"</warning>
