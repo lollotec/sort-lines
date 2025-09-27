33e063b59d40b9f6719d5af1993f8125b555877c
Jodie Weldon <weldon.jodie@gmail.com>
2025-09-27 15:48:53 -0700

    Fix tests (again)

--- src/test/testData/highlight/InvalidSortOrderHighlighting.kt
+++ src/test/testData/highlight/InvalidSortOrderHighlighting.kt
@@ -1,11 +1,15 @@
+package test
+
+import kotlin.plus
+
 class InvalidSortOrderHighlighting {
     // sort: <error descr="Invalid sort order">error</error>
     val apple = "zebra"
     val pear = "bear"
 
     // sort: <error descr="Invalid sort order">{ order: error, group: /\(.*\)/ }</error>
-    fun add(a: Int) = a
-    fun add(a: Int, b: Int) = a.plus(b)
-    fun add(a: Int, b: Int, c: Int) = a.plus(b).plus(c)
+    val notFirst = arrayOf<String>("apple")
+    val isSecond = arrayOf<String>("apple", "beetle")
+    val notLast = arrayOf<String>("apple", "beetle", "bee")
 
 }
