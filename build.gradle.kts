519463dbd7916efb06fd954b4d55feca05a30a9c
Jodie Weldon <weldon.jodie@gmail.com>
2025-10-19 19:26:34 -0700

    Add gradle tasks for generating the lexer and parser

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.qodana) // Gradle Qodana Plugin
     alias(libs.plugins.kover) // Gradle Kover Plugin
+    alias(libs.plugins.grammarkit)
 }
 
 group = providers.gradleProperty("pluginGroup").get()
@@ -137,6 +138,26 @@ kover {
 }
 
 tasks {
+    generateParser {
+        sourceFile.set(file("src/main/grammars/Sort.bnf"))
+        pathToParser.set("com/github/jodiew/sortlines/lang/parser/SortParser.java")
+        pathToPsiRoot.set("com/github/jodiew/sortlines/lang/psi")
+        targetRootOutputDir.set(file("src/main/gen"))
+        purgeOldFiles.set(true)
+    }
+
+    generateLexer {
+        sourceFile.set(file("src/main/grammars/Sort.flex"))
+        targetOutputDir.set(file("src/main/gen/com/github/jodiew/sortlines/lang"))
+        purgeOldFiles.set(false)
+
+        dependsOn(generateParser)
+    }
+
+    compileKotlin {
+        dependsOn(generateLexer)
+    }
+
     wrapper {
         gradleVersion = providers.gradleProperty("gradleVersion").get()
     }
