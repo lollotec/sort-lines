005820104bbb3e5cff9018dab950854918e4cb3b
Jodie Weldon <weldon.jodie@gmail.com>
2025-10-26 16:22:02 -0700

    Add error annotations for group and split regex

--- src/test/testData/annotation/InvalidGroupOptionHighlighting.kt
+++ src/test/testData/annotation/InvalidGroupOptionHighlighting.kt
@@ -7,7 +7,7 @@ class InvalidGroupOptionHighlighting {
     val c = "third"
     val d = "fourth"
 
-    // sort: { order: asc, group: /<error descr="Invalid group pattern">[</error>/ }
+    // sort: { order: asc, group: /<error descr="Invalid group regex">[</error>/ }
     val e = "fifth"
     val f = "sixth"
 }
