9353a0215bf8dcaad9aa2efc2fa55b9d81e2cd2e
Jodie Weldon <weldon.jodie@gmail.com>
2025-11-28 17:21:22 -0800

    Upgrade dependencies and small fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.changelog.Changelog
-import org.jetbrains.changelog.ChangelogSectionUrlBuilder
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
@@ -40,14 +39,14 @@ repositories {
     }
 }
 
-// Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
+// Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/version_catalogs.html
 dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.opentest4j)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+        intellijIdea(providers.gradleProperty("platformVersion"))
 
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
@@ -110,7 +109,7 @@ intellijPlatform {
         token = providers.environmentVariable("PUBLISH_TOKEN")
         // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        // https://plugins.jetbrains.com/docs/intellij/publishing-plugin.html#specifying-a-release-channel
         channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
     }
 
@@ -131,29 +130,10 @@ intellijPlatform {
 changelog {
     groups.empty()
     repositoryUrl = providers.gradleProperty("pluginRepositoryUrl")
-    sectionUrlBuilder = ChangelogSectionUrlBuilder { repositoryUrl, currentVersion, previousVersion, isUnreleased ->
-        val prefix: (String?) -> String = { version -> when (version) {
-            "1.0.0" -> ""
-            "1.0.1" -> ""
-            "1.0.2" -> ""
-            else -> "v"
-        } }
-        val previousPrefix = prefix(previousVersion)
-        val currentPrefix = prefix(currentVersion)
-        repositoryUrl + when {
-            isUnreleased -> when (previousVersion) {
-                null -> "/commits"
-                else -> "/compare/$previousPrefix$previousVersion...HEAD"
-            }
-
-            previousVersion == null -> "/commits/$currentPrefix$currentVersion"
-
-            else -> "/compare/$previousPrefix$previousVersion...$currentPrefix$currentVersion"
-        }
-    }
+    versionPrefix = ""
 }
 
-// Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
+// Configure Gradle Kover Plugin - read more: https://kotlin.github.io/kotlinx-kover/gradle-plugin/#configuration-details
 kover {
     reports {
         total {
